78bc1683562d8f7388f0b1de37e1f381c4cd16aa
ingdavann <ingdavann4444@gmail.com>
2024-03-28 06:46:30 +0700

    created: generated two table is accounts and account_types

--- src/main/java/co/istad/vannbankingapi/domain/Account.java
+++ src/main/java/co/istad/vannbankingapi/domain/Account.java
@@ -1,4 +1,32 @@
 package co.istad.vannbankingapi.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "accounts")
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 9)
+    private String actNo;
+
+    @Column(unique = true, nullable = true, length = 100)
+    private String actName;
+
+    private BigDecimal transferLimit;
+
+    // Account has a type
+    @ManyToOne
+    private AccountType accountType;
+
 }
--- src/main/java/co/istad/vannbankingapi/domain/AccountType.java
+++ src/main/java/co/istad/vannbankingapi/domain/AccountType.java
@@ -1,4 +1,30 @@
 package co.istad.vannbankingapi.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "account_types")
 public class AccountType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false, length = 100)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private Boolean isDeleted;
+
+    @OneToMany(mappedBy = "accountType")
+    private List<Account> account;
 }
